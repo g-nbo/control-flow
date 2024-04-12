4a0ba3c3ce5c64a051f062fc4724bdab10216aad
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-04-11 20:19:43 -0400

    Complete Part 2: Thinking Bigger

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let takenSpace = 0;
 let totalPlants = startingPlants;
 
 
-/// Predict the plant growth after a specific number of weeks
+/// For loop to predict the plant growth after a specific number of weeks
 for (i = 1; i <= weeks; i++) {
     takenSpace += plantReqSpace * startingPlants * 2;
     totalPlants *= 2;
@@ -20,6 +20,8 @@ for (i = 1; i <= weeks; i++) {
     console.log(`============================================`)
     console.log(`There's (${takenSpace} out of ${area}) sq meters of taken Space after ${i} week(s) of growth `);
     console.log(`There are ${totalPlants} Total Plants after ${i} week(s) of growth `);
+
+    // Implement control flow to make decisions on how to handle the growth of the plants
     if (totalPlants <= 49) {
         console.log(`Planted, there is more room to plant more plants.`)
     } else if (totalPlants > 49  && totalPlants <= 78) {
@@ -28,29 +30,39 @@ for (i = 1; i <= weeks; i++) {
         console.log(`Pruned, stop them from exceeding the capacity of the garden.`)
     }
     console.log(`============================================`)
-}
+} // Week 1: Plant more, Week 2: Pruned, Week 3: Pruned.
 
-// Implement control flow to make decisions on how to handle the growth of the plants
+
 
 
 let totalSpace = 0;
-let newPlants = 0;
 let newStartingPlants = 100;
+let newPlants = newStartingPlants;
 
 // Part 2: Thinking Bigger
-// function additionalSpace() {
-//     for (i = 1; i <= 10; i++) {
-//         totalSpace += plantReqSpace * newStartingPlants * 2;
-//         newPlants += 100 * 2;
-//         console.log(`Week ${i} Results: `)
-//         console.log(`============================================`)
-//         console.log(`There's (${totalSpace} sq meters taken after ${i} week(s) of growth `);
-//         console.log(`There are ${newPlants} Total Plants after ${i} week(s) of growth `);
-//         console.log(`============================================`)
-//     }
-// }
 
-// additionalSpace();
+//The amount of additional space required if the scientist start with 100 and did not prune for 10 weeks
+function additionalSpace() {
+    for (i = 1; i <= 10; i++) {
+        totalSpace += plantReqSpace * newStartingPlants * 2;
+        newPlants *= 2;
+        console.log(`Week ${i} Results: `)
+        console.log(`============================================`)
+        console.log(`There's (${totalSpace} sq meters taken after ${i} week(s) of growth `);
+        console.log(`There are ${newPlants} Total Plants after ${i} week(s) of growth `);
+        console.log(`============================================`)
+    }
+
+    // The radius of the new expanded garden
+    let newRadius = Math.sqrt(1600 / PI);
+    let additionalSpace = totalSpace - area;
+    console.log(`New radius of the circle is equal to ${newRadius}.`);
+    console.log(`Additional Space required = ${additionalSpace}.`);
+
+}
+
+additionalSpace(); // There are 1600 sq meters taken and 102400 total plants after 10 weeks of growth
+                    // Additional space = 
 
 
 
