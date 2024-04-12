a4d5859281879c46c3a2777e9e15f21b48c0fefc
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-04-11 20:02:55 -0400

    Fixed logic error in Part 1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//Declaring given variables
+// Part 1: Growing Pains
+
+// Declaring given variables
 const radius = 5;
 const PI = 3.1415;
 const area = PI * radius * radius;
@@ -7,13 +9,13 @@ const startingPlants = 20;
 
 const weeks = 3; // 3 weeks of inputs for console.log results (check console for results)
 let takenSpace = 0;
-let totalPlants = 0;
+let totalPlants = startingPlants;
 
 
 /// Predict the plant growth after a specific number of weeks
 for (i = 1; i <= weeks; i++) {
     takenSpace += plantReqSpace * startingPlants * 2;
-    totalPlants += startingPlants * 2;
+    totalPlants *= 2;
     console.log(`Week ${i} Results: `)
     console.log(`============================================`)
     console.log(`There's (${takenSpace} out of ${area}) sq meters of taken Space after ${i} week(s) of growth `);
@@ -30,7 +32,24 @@ if (totalPlants <= 49) {
     console.log(`Pruned, stop them from exceeding the capacity of the garden.`)
 }
 
+let totalSpace = 0;
+let newPlants = 0;
+let newStartingPlants = 100;
 
+// Part 2: Thinking Bigger
+// function additionalSpace() {
+//     for (i = 1; i <= 10; i++) {
+//         totalSpace += plantReqSpace * newStartingPlants * 2;
+//         newPlants += 100 * 2;
+//         console.log(`Week ${i} Results: `)
+//         console.log(`============================================`)
+//         console.log(`There's (${totalSpace} sq meters taken after ${i} week(s) of growth `);
+//         console.log(`There are ${newPlants} Total Plants after ${i} week(s) of growth `);
+//         console.log(`============================================`)
+//     }
+// }
+
+// additionalSpace();
 
 
 
